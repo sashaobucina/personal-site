8ee5e09f73dc8685fdfe88e4b1b1e0401d7b68b8
Sasha Obucina <sasha.obucina@gmail.com>
2019-05-14 18:19:11 -0400

    Fix header bug

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import Landing from './Landing';
-import Header from './Header';
 
 function App() {
   return (
     <div className="App">
-      <Header />
       <Landing />
     </div>
   );
