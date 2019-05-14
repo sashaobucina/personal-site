3003087d20f2b88623f67dc2907ce77039b708a0
Sasha Obucina <sasha.obucina@gmail.com>
2019-05-14 15:39:19 -0400

    Finish education section

--- src/components/Education.js
+++ src/components/Education.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { MDBContainer, MDBJumbotron, MDBRow, MDBCol } from "mdbreact";
+import { MDBContainer, MDBJumbotron, MDBRow, MDBCol, MDBListGroup, MDBListGroupItem } from "mdbreact";
 import CustomDivider from "./CustomDivider";
 
 class Education extends Component {
@@ -11,6 +11,25 @@ class Education extends Component {
             <MDBCol md="12">
               <h3>Education</h3>
               <CustomDivider />
+            </MDBCol>
+          </MDBRow>
+          <MDBRow className="mt-3">
+            <MDBCol className="offset-md-1" md="10">
+              <MDBListGroup>
+                <MDBListGroupItem style={{ textAlign: "left" }}>
+                  <div className="d-flex w-100 justify-content-between">
+                    <h4 className="mb-2 font-weight-bolder">University of Toronto</h4>
+                    <p className="text-muted mb-2">September 2015 - Present</p>
+                  </div>
+                  <p className="text-muted mb-1 font-weight-bolder" style={{ fontSize: "16px" }}>Computer Science and Economics</p>
+                  <p className="text-muted mb-3" style={{ fontSize: "16px" }}>Toronto, Canada</p>
+                  <p>
+                    Currently pursuing a double degree program at the University of Toronto at the St. George Campus for Computer Science and Economics. 
+                    I took a professional experience year to gain work experience through technical internships and therefore will be 
+                    graduating in five years instead of four.
+                  </p>
+                </MDBListGroupItem>
+              </MDBListGroup>
             </MDBCol>
           </MDBRow>
         </MDBContainer>
