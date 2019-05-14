bf465c29ed3f4847713d0b2545fd9174396370cf
Sasha Obucina <sasha.obucina@gmail.com>
2019-05-14 03:27:13 -0400

    Fix typos

--- src/components/About.js
+++ src/components/About.js
@@ -20,9 +20,9 @@ class About extends Component {
               I'm Sasha! A passionate CS undergraduate at the <a href="https://www.utoronto.ca/">University of Toronto (St. George)</a>.
               My interest in the field of computer science started because of the impact software has on our lives, especially keen on machine learning and cybersecurity.
               I have a passion for big challenges that require innovative thinking, speed, and creativity to tackle.
-              I would describe myself as a self-starter with experience in natural language processing, cybersecurty, and backend web development, amassed through school and my internship opportunities.
+              I would describe myself as a self-starter with experience in natural language processing, cybersecurity, and backend web development, amassed through experiences at school and my internships.
               Along with my experience in computer science, I am majoring in Economics as well, in which I am constantly searching for software solutions applicable to the business world.
-              I am also a competitive tennis player, currenlty a member of the Varsity Men's tennis team and a former member of the national team.
+              I am also a competitive tennis player, currently a member of the UofT Varsity Men's tennis team and a former member of the national team.
             </p>
           </MDBCol>
         </MDBRow>
